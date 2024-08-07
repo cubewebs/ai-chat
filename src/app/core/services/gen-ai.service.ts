01e7bad5f858f7929e6b2845da4ad9c175994ea6
cubewebs <juanjoseaos@hotmail.com>
2024-08-07 21:47:23 +0100

    docs

--- src/app/core/services/gen-ai.service.ts
+++ src/app/core/services/gen-ai.service.ts
@@ -1,5 +1,5 @@
 import {Injectable, signal} from '@angular/core';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
 @Injectable({
